44314764b64bfa0e37237e128a1ac297495eed65
anita-sharma <2016uce1171@mnit.ac.in>
2020-07-26 14:00:32 +0530

    Updates

---
+++ precache-manifest.8217f94bd45e52c28bf2fe57c338e1dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df80afe63bffa6a9c99ea3f06c313b57",
+    "url": "/cardgame/index.html"
+  },
+  {
+    "revision": "12434a177a43db895f90",
+    "url": "/cardgame/static/css/main.a57d9fb1.chunk.css"
+  },
+  {
+    "revision": "c470d0915d297dd5fe40",
+    "url": "/cardgame/static/js/2.0e76ec9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cardgame/static/js/2.0e76ec9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12434a177a43db895f90",
+    "url": "/cardgame/static/js/main.a831d563.chunk.js"
+  },
+  {
+    "revision": "3e1f892a47a9826d4d33",
+    "url": "/cardgame/static/js/runtime-main.b26410fb.js"
+  },
+  {
+    "revision": "df787e08f3d745e70ede1db15eaef6fb",
+    "url": "/cardgame/static/media/15.df787e08.jpeg"
+  }
+]);
